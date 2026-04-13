e670ebca4b638d1f2ccabb8aa6c32a0780196cfc
Informalinx <muller-lucas@outlook.fr>
2026-04-13 12:49:11 +0200

    build: install and configure i18n package

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -10,7 +11,13 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/informalinx/blog/internal/env"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+	"gopkg.in/yaml.v3"
 )
+
+// Protect with sync/mutex if modified inside mutliples http handlers or goroutines
+var userLocale = language.English.String()
 
 func main() {
 	conf, err := env.Load(".env")
@@ -22,6 +29,17 @@ func main() {
 
 	_ = validator.New()
 
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("yaml", yaml.Unmarshal)
+	msgFile, err := bundle.LoadMessageFile("./translations/messages.en.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := bundle.AddMessages(language.English, msgFile.Messages...); err != nil {
+		log.Fatal(err)
+	}
+
 	db, err := sql.Open(conf.DatabaseDriver, conf.DatabaseDSN)
 	if err != nil {
 		log.Fatalf("error while connecting to database : %s", err)
@@ -31,10 +49,40 @@ func main() {
 		log.Fatalf("cannot ping database : %s", err)
 	}
 
+	localizer := i18n.NewLocalizer(bundle, userLocale)
+
+	funcMap := template.FuncMap{
+		"trans": func(params any, message string) string {
+			return localizer.MustLocalize(&i18n.LocalizeConfig{
+				MessageID:    message,
+				TemplateData: params,
+			})
+		},
+		"dict": func(values ...any) map[any]any {
+			if len(values)%2 != 0 {
+				panic("map : expected an even number of arguments (key/value pairs)")
+			}
+
+			result := make(map[any]any, len(values)/2)
+			for i, val := range values {
+				if i%2 != 0 {
+					result[values[i-1]] = val
+				}
+			}
+
+			return result
+		},
+	}
+
+	baseTmpl := template.Must(template.New("index.html").Funcs(funcMap).ParseFiles("./website/templates/index.html"))
+
 	mux := http.NewServeMux()
 
 	mux.Handle("/{$}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World !"))
+		data := struct{ Count int }{Count: 10}
+		if err := baseTmpl.Execute(w, data); err != nil {
+			log.Fatal(err)
+		}
 	}))
 
 	fmt.Println("Server listening on :", conf.ServerAddress)
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/informalinx/blog
 go 1.25.6
 
 require (
+	github.com/go-playground/validator/v10 v10.30.2
+	github.com/gorilla/sessions v1.4.0
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.42
+	github.com/nicksnyder/go-i18n/v2 v2.6.1
+	golang.org/x/crypto v0.49.0
+	golang.org/x/text v0.35.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -27,7 +33,6 @@ require (
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.30.2 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
@@ -35,7 +40,6 @@ require (
 	github.com/google/cel-go v0.26.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
-	github.com/gorilla/sessions v1.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -82,18 +86,15 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.49.0 // indirect
 	golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/text v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260406210006-6f92a3bedf2d // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260401024825-9d38bb4040a9 // indirect
 	google.golang.org/grpc v1.79.3 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v1.0.1 // indirect
 	modernc.org/libc v1.68.0 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
